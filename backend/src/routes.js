a91ad9fabea33837cd6b879d4b85020fd1dfde29
Guilherme Castelo <48875867+castelogui@users.noreply.github.com>
2021-01-20 22:48:45 -0400

    add route for login contoller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const connection = require('./database/conection')
 
 const UserController = require('./controllers/userController')
+const loginController = require('./controllers/loginController')
 
 const routes = express.Router()
 
+routes.get('/login/:username/:password', loginController.login)
 routes.get('/user/:id_user', UserController.oneUser)
 
 module.exports = routes;
