c5ce8e2d2a0ee147b73e82460f88541f6ce15ddc
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-12 15:06:51 -0400

    add component capa

---
+++ web/src/pages/Main/Components.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as Logo } from "../assets/images/illustrations/loginpage.svg";
+
+const Capa = () => {
+  return <Logo />;
+};
+
+Logo = styled.img`
+  width: 10vw;
+`;
+
+export default Capa;
